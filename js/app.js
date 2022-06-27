04678142a9edac476fbfc071613ebb2fb5431a5e
Alessandra Arouca <alexia.arouca@gmail.com>
2022-06-27 19:04:10 -0300

    Finalizando o projeto

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import { valida } from `./validate.js`
+
+const inputs = document.querySelectorAll("inpput")
+
+inputs.forEach(input => {
+    input.addEventListener("valida(input:any"): void
+        valida(evento.target)
+})
